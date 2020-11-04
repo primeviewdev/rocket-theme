b5c2c9046b2c26121fa4dd544af42c46687e0e51
zekinah <zjlecaros@gmail.com>
2020-11-03 17:18:53 -0700

    Added Woocommerce product gallery support

--- includes/theme-settings/asset-import.php
+++ includes/theme-settings/asset-import.php
@@ -137,9 +137,28 @@ function wsds_async_scripts( $tag, $handle, $src ) {
     }
     return $tag;
 }
-function zn_upload_mimes($mimes) {
+function rocket_upload_mimes($mimes) {
 	$mimes['csv'] = "text/csv";
 	return $mimes;
 }
-add_filter('upload_mimes', 'zn_upload_mimes');
+add_filter('upload_mimes', 'rocket_upload_mimes');
 
+/**
+ * Woocommerce Support
+ */
+function rocket_add_woocommerce_support() {
+	add_theme_support( 'woocommerce', array(
+        'product_grid'          => array(
+            'default_rows'    => 4,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 3,
+            'min_columns'     => 2,
+            'max_columns'     => 6,
+        ),
+    ) );
+    add_theme_support('wc-product-gallery-zoom');
+    add_theme_support('wc-product-gallery-lightbox');
+    add_theme_support('wc-product-gallery-slider');
+}
+add_action( 'after_setup_theme', 'rocket_add_woocommerce_support' );
